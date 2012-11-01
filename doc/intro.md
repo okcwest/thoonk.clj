01f0e31aeec0f6433502ebe62c624ff72dbde557
Chandra Barnett <barnett@okcupidlabs.com>
2012-11-01 16:45:04 -0700

    Now with directories!

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to thoonk
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/thoonk/core_test.clj
@@ -0,0 +1,7 @@
+(ns thoonk.core-test
+  (:use clojure.test
+        thoonk.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
