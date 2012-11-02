9b39d1f3e56770772f857d00bf2a5ab0f0424584
Trenton Strong <strong@okcupidlabs.com>
2012-11-02 11:59:09 -0700

    Feed record and default keys constructor

--- src/thoonk/feeds/feed.clj
+++ src/thoonk/feeds/feed.clj
@@ -1,6 +1,7 @@
-(ns thoonk.feeds.feed)
+(ns thoonk.feeds.feed
+  (:require [taoensso.carmine :as redis]))
 
-(dfeprotocol FeedP
+(defprotocol FeedP
 	(get-channels [])
 	(delete-feed [])
 	(get-schemas [])
@@ -9,3 +10,21 @@
 	(get-all [])
 	(publish [item] [item id])
 	(retract [id]))
+
+(defrecord Feed [name feed-ids feed-items feed-publish
+                 feed-publishes feed-retract feed-config
+                 feed-edit])
+
+(defn make-feed
+  [name]
+  (Feed. name
+         (str "feed.ids:" name)
+         (str "feed.items:" name)
+         (str "feed.publish:" name)
+         (str "feed.publishes:" name)
+         (str "feed.retract:" name)
+         (str "feed.config:" name)
+         (str "feed.edit:" name)))
+
+           
+
