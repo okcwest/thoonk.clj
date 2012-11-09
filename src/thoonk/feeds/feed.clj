3505990ff78b02233587b6205d57391e1ccd57a3
Chandra Barnett <barnett@okcupidlabs.com>
2012-11-09 13:51:14 -0800

    First-pass implementations of Feed and Queue, untested, probably broken

--- src/thoonk/feeds/feed.clj
+++ src/thoonk/feeds/feed.clj
@@ -1,19 +1,74 @@
 (ns thoonk.feeds.feed
-  (:require [taoensso.carmine :as redis]))
+  (:require [taoensso.carmine :as redis])
+  (:import (java.util UUID)))
+
+(def pool         (redis/make-conn-pool)) ; See docstring for additional options
+(def spec-server1 (redis/make-conn-spec)) ; ''
+(defmacro wredis [& body] `(redis/with-conn pool spec-server1 ~@body))
 
 (defprotocol FeedP
-	(get-channels [])
-	(delete-feed [])
-	(get-schemas [])
-	(get-ids [])
-	(get-item [] [id])
-	(get-all [])
-	(publish [item] [item id])
-	(retract [id]))
+    (get-channels [])
+    (delete-feed [])
+    (get-schemas [])
+    (get-ids [])
+    (get-item [] [id])
+    (get-all [])
+    (publish [this item] [this item args])
+    (retract [id]))
+
 
 (defrecord Feed [name feed-ids feed-items feed-publish
                  feed-publishes feed-retract feed-config
-                 feed-edit])
+                feed-edit]
+    FeedP
+        ; thoonk api
+        (get-channels [this]
+            [(:feed-publish this) (:feed-retract this) (:feed-edit this)])
+        (get-schemas [this]
+            #{(:feed-ids this) (:feed-items this) (:feed-publish this) 
+                (:feed-publishes this) (:feed-retract this) (:feed-config this)
+                (:feed-edit this)})
+        ;(delete-feed [this]
+            ; python client does this via the thoonk utility
+            ;(thoonk/delete (:feed-ids this)))
+        ; standard api
+        (get-ids [this]
+            (wredis (redis/zrange (:feed-ids this) 0 -1)))
+        (get-item [this]
+            (get-item this (wredis (redis/lindex (:feed-ids this) 0))))
+        (get-item [this id]
+            (wredis (redis/hget (:feed-items this) id)))
+        (get-all [this]
+            (wredis (redis/hgetall (:feed-items this))))
+        (publish [this item args]
+            (let [  id (:id args)
+                    maxlen (hget pipe (:feed-config this) "max-length") 
+                    delete-ids (if (or (nil? maxlen) (equal? 0 maxlen)) 
+                        [] ; no stale items
+                        (wredis (redis/zrange (:feed-ids this) 0 (- max 0))))]
+                    (wredis ; pipeline the following together
+                        (redis/multi) ; open a transaction block
+                        (doseq [delete-id] delete-ids
+                            (if (not (equal? delete-id id)) ; retract any stale items that were there
+                                (redis/zrem (:feed-ids this) delete-id)
+                                (redis/hget (:feed-items this) delete-id)
+                                (thoonk/publish (:feed-retract this) delete-id)))
+                        (redis/zadd (:feed-ids this) $TIME$) ; time-order the item's id
+                        (redis/incr (:feed-publishes this))
+                        (redis/hset (:feed-items this) id item) ; push the actual item
+                        (redis/exec) ; commit the transaction
+                    ))
+            ))
+        (publish [this item]
+            (publish this (UUID/randomUUID)))
+        (retract [this id]
+            (if (not (nil? (wredis redis/zrank (:feed-ids this) id)))
+                (wredis 
+                    (redis/multi)
+                    (redis/zrem (:feed-ids this) id)
+                    (redis/hdel (:feed-items this) id)
+                    (thoonk/publish (:feed-retract this) id)
+                    (redis/exec))))
 
 (defn make-feed
   [name]
@@ -25,6 +80,3 @@
          (str "feed.retract:" name)
          (str "feed.config:" name)
          (str "feed.edit:" name)))
-
-           
-
---
+++ src/thoonk/feeds/queue.clj
@@ -0,0 +1,74 @@
+(ns thoonk.feeds.queue
+    (:require [taoensso.carmine :as redis])
+    (:import (java.util UUID)))
+
+; a specific protocol for the queue feed type
+(defprotocol QueueP
+    ; queue-specific implementations
+    (put [this item] [this item priority])
+    (get [this] [this timeout])
+    (get-ids [this]))
+
+(defrecord Queue [name feed]
+    FeedP
+        ; we alias publish to put, for queues.
+        (publish [this item]
+            (publish this item false))
+        ; we implement just about everything else by handing it off to the encapsulated feed
+        (get-channels [this]
+            (get-channels (:feed this)))
+        (delete-feed [this]
+            (delete-feed (:feed this)))
+        (get-schemas [this]
+            (get-schemas (:feed this)))
+        (get-ids [this]
+            (get-schemas (:feed this)))
+        (get-item [this]
+            (get-item (:feed this)))
+        (get-item [this id]
+            (get-item (:feed this) id))
+        (get-all [this]
+            (get-all (:feed this)))
+        (publish [this item]
+            (publish (:feed this) item)) 
+        (publish [this item args]
+            (if (nil? (:id args))
+                (put (:feed this) item (:priority args)))
+                (publish (:feed this) item (:id args)))
+        (retract [this id]
+            (retract (:feed this) id)))
+        
+    QueueP
+        (publish [this item priority]
+            (put this item priority))
+        (put [this item priority]
+            (let [id (java.util.UUID/randomUUID)]
+                (wredis 
+                    (redis/multi)
+                    (if priority
+                        (do
+                            (redis/rpush (:feed-ids (:feed this)) id)
+                            (redis/hset (:feed-items (:feed this)) id item)
+                            (redis/incr (:feed-publishes (:feed this))))
+                        (do
+                            (redis/rpush (:feed-ids (:feed this)) id)
+                            (redis/hset (:feed-items (:feed this)) id item)
+                            (redis/incr (:feed-publishes (:feed this)))))
+                    (redis/exec))))
+        (put [this item]
+            (put this item false))
+        (get [this timeout]
+            (let [result (wredis (redis/brpop (:feed-ids (:feed this)) timeout))]
+                (if (nil? result)
+                    nil
+                    (first
+                        (let [id (nth 2 result)]
+                            (wredis
+                                (redis/multi)
+                                (redis/hget (:feed-items (:feed this)) id)
+                                (redis/hdel (:feed-items (:feed this)) id)
+                                (redis/exec)))))))
+        (get [this]
+            (get this 0))
+        (get-ids [this]
+            (wredis (redis/lrange (:feed-ids (:feed this)) 0 -1))))
