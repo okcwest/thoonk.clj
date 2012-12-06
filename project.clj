8f5083cca41fe48d51d43b89a18070cece653a50
Chandra Barnett <barnett@okcupidlabs.com>
2012-12-06 12:13:20 -0800

    log4j jiggerypokery

--- project.clj
+++ project.clj
@@ -4,5 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.taoensso/carmine "0.11.3"]]
-  :aot [#"thoonk.exceptions.*"])
+                 [com.taoensso/carmine "0.11.3"]
+                 [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jdmk/jmxtools
+                                                    com.sun.jmx/jmxri]]
+                 [org.clojure/tools.logging "0.2.3"]]
+  :aot [#"thoonk.exceptions.*"]
+  :source-paths ["src"]
+  :test-paths ["test"]
+  )
